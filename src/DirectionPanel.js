a15686bbe7653a94b90830a1608a332433d57077
Bohdan Sadovyak <bohdan.sadovyak@gmail.com>
2023-07-06 11:09:36 +0300

    Modernizes class DirectionPanel

--- src/DirectionPanel.js
+++ src/DirectionPanel.js
@@ -1,46 +1,48 @@
-/* global PIXI */
+import {Graphics, Sprite, Texture, Text} from "pixi.js";
 
-/**
- * @constructor
- * @param {Config} config
- */
-function DirectionPanel(config) {
-  PIXI.Graphics.apply(this);
+export default class DirectionPanel extends Graphics {
+  _textures = {};
+  _sprite = null;
+  _label = null;
 
-  this._theme = config.directionPanel;
+  constructor(config) {
+    super();
 
-  this._textures = {};
-  this._textures[0] = PIXI.Texture.fromImage('img/direction-0.png');
-  this._textures[1] = PIXI.Texture.fromImage('img/direction-1.png');
-  this._textures[2] = PIXI.Texture.fromImage('img/direction-2.png');
-  this._textures[3] = PIXI.Texture.fromImage('img/direction-3.png');
-  this._sprite = this.addChild(new PIXI.Sprite(this._textures[0]));
-  this._sprite.alpha = 0.9;
-  this._sprite.anchor.x = 0.5;
-  this._sprite.anchor.y = 0.5;
+    this._config = config;
 
-  this._label = this.addChild(new PIXI.MultiStyleText('...', this._theme._label));
-  this._label.x = this._sprite.x + 40;
-  this._label.anchor.y = 0.5;
-  this._live = false;
-  /** @type {PlayerInfo} this._player */
-  this._player = null;
-}
+    this._textures[0] = Texture.from('img/direction-0.png');
+    this._textures[1] = Texture.from('img/direction-1.png');
+    this._textures[2] = Texture.from('img/direction-2.png');
+    this._textures[3] = Texture.from('img/direction-3.png');
+    this._sprite = this.addChild(new Sprite(this._textures[0]));
+    this._sprite.alpha = 0.9;
+    this._sprite.anchor.x = 0.5;
+    this._sprite.anchor.y = 0.5;
 
-DirectionPanel.prototype = Object.create(PIXI.Graphics.prototype);
-
-DirectionPanel.prototype.setAngle = function (angle) {
-  if (this._live) {
-    this._sprite.rotation = angle;
+    this._label = this.addChild(new Text('...', this._config.label.def));
+    this._label.x = this._sprite.x + 40;
+    this._label.anchor.y = 0.5;
+    this._live = false;
+    this._player = null;
   }
-};
 
-DirectionPanel.prototype.update = function () {
-  if (this._player) {
-    this._sprite.texture = this._textures[this._player._status & 3];
-    this._live = !!(this._player._status & 2);
-    if (!this._live) {
-      this._sprite.rotation = 0;
+  set angle(value) {
+    if (this._live) {
+      this._sprite.rotation = value;
     }
   }
-};
+
+  set player(value) {
+    this._player = value;
+  }
+
+  update() {
+    if (this._player) {
+      this._sprite.texture = this._textures[this._player.status & 3]; // TODO: avoid magic numbers
+      this._live = !!(this._player.status & 2); // TODO: avoid magic numbers
+      if (!this._live) {
+        this._sprite.rotation = 0;
+      }
+    }
+  }
+}
