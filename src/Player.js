5cb07e677a66e1a9bb57ee8a63c5a49639dd879a
Bohdan Sadovyak <bohdan.sadovyak@gmail.com>
2023-07-07 14:21:21 +0300

    Modernizes class Player

--- src/Player.js
+++ src/Player.js
@@ -1,52 +1,43 @@
-/* global PIXI */
+export default class Player {
+  _id = 0;
+  _x = 0;
+  _y = 0;
+  _mass = 0;
+  _avatars = new Set();
 
-/** @constructor */
-function Player() {
-  this._id = 0;
-  this._x = 0;
-  this._y = 0;
-  this._mass = 0;
-  this._avatars = new Set();
+  addAvatar(avatar) {
+    this._avatars.add(avatar);
+  };
+
+  removeAvatar(avatar) {
+    this._avatars.delete(avatar);
+  };
+
+  clearAvatars() {
+    this._avatars.clear();
+  };
+
+  update() {
+    let size = this._avatars.size;
+    if (!size) {
+      return;
+    }
+    if (size > 1) {
+      this._x = 0;
+      this._y = 0;
+      this._mass = 0;
+      this._avatars.forEach((avatar) => {
+        this._x += avatar._position._x * avatar._mass;
+        this._y += avatar._position._y * avatar._mass;
+        this._mass += avatar._mass;
+      }, this);
+      this._x /= this._mass;
+      this._y /= this._mass;
+    } else {
+      let avatar = this._avatars.values().next().value;
+      this._mass = avatar._mass;
+      this._x = avatar._position._x;
+      this._y = avatar._position._y;
+    }
+  };
 }
-
-/**
- * @param {Avatar} avatar
- */
-Player.prototype.addAvatar = function (avatar) {
-  this._avatars.add(avatar);
-};
-
-/**
- * @param {Avatar} avatar
- */
-Player.prototype.removeAvatar = function (avatar) {
-  this._avatars.delete(avatar);
-};
-
-Player.prototype.clearAvatars = function () {
-  this._avatars.clear();
-};
-
-Player.prototype.$update = function () {
-  var size = this._avatars.size;
-  if (!size) {
-    return;
-  }
-  if (size > 1) {
-    this._x = 0;
-    this._y = 0;
-    this._mass = 0;
-    this._avatars.forEach(function (avatar) {
-      this._x += avatar._position._x * avatar._mass;
-      this._y += avatar._position._y * avatar._mass;
-      this._mass += avatar._mass;
-    }, this);
-    this._x /= this._mass;
-    this._y /= this._mass;
-  } else {
-    var avatar = this._avatars.values().next().value;
-    this._mass = avatar._mass;
-    this._x = avatar._position._x;
-    this._y = avatar._position._y;
-  }
-};
