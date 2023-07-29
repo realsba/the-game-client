4db4330fce63b8554fde62fea7e968994f256578
Bohdan Sadovyak <bohdan.sadovyak@gmail.com>
2023-07-29 12:10:28 +0300

    Added tests for class BinaryStream

--- src/BinaryStream.js
+++ src/BinaryStream.js
@@ -1,71 +1,78 @@
 export default class BinaryStream {
-  _offset = 0;
-  _dataview = null;
-  _uint8Array = null;
+  #offset = 0;
+  #dataview = null;
+  #uint8Array = null;
 
   constructor(buffer) {
-    this._dataview = new DataView(buffer);
-    this._uint8Array = new Uint8Array(buffer);
+    if (!buffer) {
+      buffer = new ArrayBuffer(32);
+    }
+    this.#dataview = new DataView(buffer);
+    this.#uint8Array = new Uint8Array(buffer);
   }
 
   get buffer() {
-    return this._dataview.buffer.slice(0, this._offset);
+    return this.#dataview.buffer.slice(0, this.#offset);
+  }
+
+  seek(offset) {
+    this.#offset = offset;
   }
 
   readUInt8() {
-    return this._dataview.getUint8(this._offset++);
+    return this.#dataview.getUint8(this.#offset++);
   };
 
   readUInt16() {
-    const value = this._dataview.getUint16(this._offset);
-    this._offset += 2;
+    const value = this.#dataview.getUint16(this.#offset);
+    this.#offset += 2;
     return value;
   };
 
   readUInt32() {
-    const value = this._dataview.getUint32(this._offset);
-    this._offset += 4;
+    const value = this.#dataview.getUint32(this.#offset);
+    this.#offset += 4;
     return value;
   };
 
   readFloat() {
-    const value = this._dataview.getFloat32(this._offset);
-    this._offset += 4;
+    const value = this.#dataview.getFloat32(this.#offset);
+    this.#offset += 4;
     return value;
   };
 
   readString() {
     const decoder = new TextDecoder();
     const length = this.readUInt16();
-    const value = decoder.decode(this._dataview.buffer.slice(this._offset, this._offset + length));
-    this._offset += length;
+    const value = decoder.decode(this.#dataview.buffer.slice(this.#offset, this.#offset + length));
+    this.#offset += length;
     return value;
   };
 
   writeUInt8(value) {
-    this._dataview.setUint8(this._offset++, value);
+    this.#dataview.setUint8(this.#offset++, value);
   };
 
   writeUInt16(value) {
-    this._dataview.setUint16(this._offset, value);
-    this._offset += 2;
+    this.#dataview.setUint16(this.#offset, value);
+    this.#offset += 2;
   };
 
   writeUInt32(value) {
-    this._dataview.setUint32(this._offset, value);
-    this._offset += 4;
+    this.#dataview.setUint32(this.#offset, value);
+    this.#offset += 4;
   };
 
   writeFloat(value) {
-    this._dataview.setFloat32(this._offset, value);
-    this._offset += 4;
+    this.#dataview.setFloat32(this.#offset, value);
+    this.#offset += 4;
   };
 
   writeString(value) {
     const encoder = new TextEncoder();
     value = encoder.encode(value);
     this.writeUInt16(value.length);
-    this._uint8Array.set(value, this._offset);
-    this._offset += value.length;
+    this.#uint8Array.set(value, this.#offset);
+    this.#offset += value.length;
   };
 }
---
+++ test/BinaryStream.test.js
@@ -0,0 +1,73 @@
+import chai from 'chai';
+import BinaryStream from '../src/BinaryStream.js';
+
+const should = chai.should();
+
+describe('BinaryStream', () => {
+  describe('UInt8', () => {
+    it('should return 255', () => {
+      const stream = new BinaryStream();
+      stream.writeUInt8(255);
+      stream.seek(0);
+      const x = stream.readUInt8();
+      x.should.equal(255);
+    });
+  });
+  describe('UInt16', () => {
+    it('should return 65535', () => {
+      const stream = new BinaryStream();
+      stream.writeUInt16(65535);
+      stream.seek(0);
+      const x = stream.readUInt16();
+      x.should.equal(65535);
+    });
+  });
+  describe('UInt32', () => {
+    it('should return 4294967295', () => {
+      const stream = new BinaryStream();
+      stream.writeUInt32(4294967295);
+      stream.seek(0);
+      const x = stream.readUInt32();
+      x.should.equal(4294967295);
+    });
+  });
+  describe('Float', () => {
+    it('should return 3.14159265359', () => {
+      const stream = new BinaryStream();
+      stream.writeFloat(3.14159265359);
+      stream.seek(0);
+      const x = stream.readFloat();
+      x.should.be.closeTo(3.14159265359, 0.000001);
+    });
+  });
+  describe('String', () => {
+    it('should return "ABCDEF$%абвгдеєж12345678"', () => {
+      const stream = new BinaryStream(new ArrayBuffer(64));
+      stream.writeString('ABCDEF$%абвгдеєж12345678');
+      stream.seek(0);
+      const x = stream.readString();
+      x.should.equal('ABCDEF$%абвгдеєж12345678');
+    });
+  });
+  describe('buffer()', () => {
+    it('should shrink result buffer', () => {
+      const stream = new BinaryStream(new ArrayBuffer(256));
+      stream.writeUInt8(1);
+      stream.writeUInt16(2);
+      stream.writeUInt32(3);
+      stream.writeFloat(4);
+      stream.buffer.byteLength.should.equal(11);
+    });
+  });
+  describe('seek()', () => {
+    it('should shrink result buffer', () => {
+      const stream = new BinaryStream(new ArrayBuffer(256));
+      stream.writeUInt8(1);
+      stream.writeUInt16(2);
+      stream.writeUInt32(3);
+      stream.writeFloat(4);
+      stream.seek(8);
+      stream.buffer.byteLength.should.equal(8);
+    });
+  });
+});
