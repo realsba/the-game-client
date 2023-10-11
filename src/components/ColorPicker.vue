48f50839cef2fc97ffe61e7faaabec104412e6e2
Bohdan Sadovyak <bohdan.sadovyak@gmail.com>
2023-10-11 18:58:26 +0300

    Implement ColorPicker

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps(['modelValue', 'colors']);
+const emit = defineEmits(['update:modelValue']);
+
+const updateSelection = (index) => {
+  emit('update:modelValue', index);
+}
+
+const isSelected = (index) => props.modelValue === index;
+</script>
+
+<template>
+  <v-row>
+    <v-col
+      v-for="(color, index) in colors"
+      :key="index"
+      md="3"
+      class="pa-1"
+    >
+      <v-btn
+        :color="color"
+        :class="{ selected: isSelected(index) }"
+        @click="updateSelection(index)"
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<style>
+.v-btn.selected {
+  border: 1px solid #000;
+}
+</style>
